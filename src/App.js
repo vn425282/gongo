70844e20547ac15f3f63bb3e33dbd1425173016a
Jackson Tri Nguyen <minhtri.nguyenit91@gmail.com>
2024-09-08 18:42:15 +0800

    Init Source Code

---
+++ src/App.js
@@ -0,0 +1,192 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='page'>
+        <Header />
+
+        <section className="section">
+          <div className="swiper-form-wrap">
+            <div className="swiper-container swiper-slider swiper-slider_height-1 swiper-align-left swiper-align-left-custom context-dark bg-gray-darker swiper-container-horizontal swiper-container-fade" data-loop="false" style={{
+    backgroundImage: "url(images/banner.jpg)"}} data-autoplay="5500" data-simulate-touch="false" data-slide-effect="fade">
+              <div className="swiper-wrapper" style={{height: "860px"}}>
+                <div className="swiper-slide swiper-slide-active" style={{background: "rgba(0, 0, 0, 0.6)"}} data-slide-bg="images/swiper-slide-1.jpg" >
+                  <div className="swiper-slide-caption">
+                    <div className="container container-bigger swiper-main-section">
+                      <div className="row row-fix justify-content-sm-center justify-content-md-start">
+                        <div className="col-md-6 col-lg-5 col-xl-4 col-xxl-5">
+                          <h3>Hundreds of Amazing Destinations</h3>
+                          <div className="divider divider-decorate"></div>
+                          <p className="text-spacing-sm">We offer a variety of destinations to travel to, ranging from exotic to some extreme ones. They include very popular countries and cities like Paris, Rio de Janeiro, Cairo and a lot of others.</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="swiper-pagination-wrap">
+                <div className="container container-bigger">
+                  <div className="row">
+                    <div className="col-sm-12">
+                      <div className="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets"><span className="swiper-pagination-bullet swiper-pagination-bullet-active"></span><span className="swiper-pagination-bullet"></span><span className="swiper-pagination-bullet"></span></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="container container-bigger form-request-wrap form-request-wrap-modern">
+              <div className="row row-fix justify-content-sm-center justify-content-lg-end">
+                <div className="col-lg-6 col-xxl-5">
+                  <div className="form-request form-request-modern bg-gray-lighter novi-background">
+                    <h4>Find your VISA Services</h4>
+                    <form className="rd-mailform form-fix" novalidate="novalidate">
+                      <div className="row row-20 row-fix">
+                        <div className="col-sm-12">
+                          <label className="form-label-outside">From</label>
+                          <div className="form-wrap form-wrap-inline">
+                            <select className="form-input select-filter select2-hidden-accessible" data-placeholder="All" data-minimum-results-for-search="Infinity" name="city" tabindex="-1" aria-hidden="true">
+                              <option value="1">New York</option>
+                              <option value="2">Lisbon</option>
+                              <option value="3">Stockholm</option>
+                            </select><span className="select2 select2-container select2-container--bootstrap" dir="ltr" style={{width: "496px"}}><span className="selection"><span className="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-labelledby="select2-city-lk-container"><span className="select2-selection__rendered" id="select2-city-lk-container" title="New York">New York</span><span className="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span className="dropdown-wrapper" aria-hidden="true"></span></span>
+                          </div>
+                        </div>
+                        <div className="col-sm-12">
+                          <label className="form-label-outside">To</label>
+                          <div className="form-wrap form-wrap-inline">
+                            <select className="form-input select-filter select2-hidden-accessible" data-placeholder="All" data-minimum-results-for-search="Infinity" name="city" tabindex="-1" aria-hidden="true">
+                              <option value="1">Chicago</option>
+                              <option value="2">Madrid</option>
+                              <option value="3">Paris</option>
+                            </select><span className="select2 select2-container select2-container--bootstrap" dir="ltr" style={{width: "496px"}}><span className="selection"><span className="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-labelledby="select2-city-fn-container"><span className="select2-selection__rendered" id="select2-city-fn-container" title="Chicago">Chicago</span><span className="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span className="dropdown-wrapper" aria-hidden="true"></span></span>
+                          </div>
+                        </div>
+                        <div className="col-sm-12 col-lg-6">
+                          <label className="form-label-outside">Depart Date</label>
+                          <div className="form-wrap form-wrap-validation">
+                            <input className="form-input" id="dateForm" name="date" type="text" data-time-picker="date" data-dtp="dtp_pQIv5" />
+                            <label className="form-label rd-input-label" for="dateForm">Choose the date</label>
+                          </div>
+                        </div>
+                        <div className="col-sm-12 col-lg-6">
+                          <label className="form-label-outside">Duration</label>
+                          <div className="form-wrap form-wrap-validation">
+                            <select className="form-input select-filter select2-hidden-accessible" data-placeholder="All" data-minimum-results-for-search="Infinity" name="city" tabindex="-1" aria-hidden="true">
+                              <option value="1">Any length</option>
+                              <option value="2">2 days</option>
+                              <option value="3">3 days</option>
+                              <option value="4">4 days</option>
+                            </select><span className="select2 select2-container select2-container--bootstrap" dir="ltr" style={{width: "233px"}}><span className="selection"><span className="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-labelledby="select2-city-yl-container"><span className="select2-selection__rendered" id="select2-city-yl-container" title="Any length">Any length</span><span className="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span className="dropdown-wrapper" aria-hidden="true"></span></span>
+                          </div>
+                        </div>
+                        <div className="col-lg-6">
+                          <label className="form-label-outside">Adults</label>
+                          <div className="form-wrap form-wrap-modern">
+                            <div className="stepper "><input className="form-input input-append stepper-input" id="form-element-stepper" type="number" min="0" max="300" value="2" /><span className="stepper-arrow up"></span><span className="stepper-arrow down"></span></div>
+                          </div>
+                        </div>
+                        <div className="col-lg-6">
+                          <label className="form-label-outside">Children</label>
+                          <div className="form-wrap form-wrap-modern">
+                            <div className="stepper "><input className="form-input input-append stepper-input" id="form-element-stepper-1" type="number" min="0" max="300" value="0" /><span className="stepper-arrow up"></span><span className="stepper-arrow down"></span></div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="form-wrap form-button">
+                        <button className="button button-block button-secondary" type="submit">Search VISA</button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="section section-lg bg-gray-lighter novi-background bg-cover text-center">
+        <div className="container container-wide">
+          <h3>our services</h3>
+          <div className="divider divider-decorate"></div>
+          <div className="row row-50 justify-content-sm-center text-left">
+            <div className="col-sm-10 col-md-6 col-xl-3">
+              <article className="box-minimal box-minimal-border">
+                <div className="box-minimal-icon novi-icon mdi mdi-airplane"></div>
+                <p className="big box-minimal-title">VISA + Air Tickets</p>
+                <hr />
+                <div className="box-minimal-text text-spacing-sm">At our travel agency, you can book air tickets to any world destination. We also provide online ticket booking via our website in just a couple of steps.</div>
+              </article>
+            </div>
+            <div className="col-sm-10 col-md-6 col-xl-3">
+              <article className="box-minimal box-minimal-border">
+                <div className="box-minimal-icon novi-icon mdi mdi-map"></div>
+                <p className="big box-minimal-title">Voyages &amp; Cruises</p>
+                <hr />
+                <div className="box-minimal-text text-spacing-sm">Besides regular tours and excursions, we also offer a variety of cruises &amp; sea voyages for different customers looking for awesome experiences.</div>
+              </article>
+            </div>
+            <div className="col-sm-10 col-md-6 col-xl-3">
+              <article className="box-minimal box-minimal-border">
+                <div className="box-minimal-icon novi-icon mdi mdi-city"></div>
+                <p className="big box-minimal-title">Hotel Bookings</p>
+                <hr />
+                <div className="box-minimal-text text-spacing-sm">We offer a wide selection of hotel ranging from 5-star ones to small properties located worldwide so that you could book a hotel you like.</div>
+              </article>
+            </div>
+            <div className="col-sm-10 col-md-6 col-xl-3">
+              <article className="box-minimal box-minimal-border">
+                <div className="box-minimal-icon novi-icon mdi mdi-beach"></div>
+                <p className="big box-minimal-title">Tailored Summer Tours</p>
+                <hr />
+                <div className="box-minimal-text text-spacing-sm">Our agency provides varied tours including tailored summer tours for clients who are searching for an exclusive and memorable vacation.</div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <section className="section section-md text-center text-md-left bg-gray-700 novi-background bg-cover">
+        <div className="container container-wide">
+          <div className="row row-fix row-50 justify-content-sm-center">
+            <div className="col-xxl-8">
+              <div className="box-cta box-cta-inline">
+                <div className="box-cta-inner">
+                  <h3 className="box-cta-title">Vietnam VISA without waiting</h3>
+                  <p>Using our website, you can book any tour just in a couple of clicks.</p>
+                </div>
+                <div className="box-cta-inner"><a className="button button-secondary button-nina" href="#">Book Now</a></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <footer className="section page-footer page-footer-minimal novi-background bg-cover text-center bg-gray-darker">
+        <div className="container container-wide">
+          <div className="row row-fix justify-content-sm-center align-items-md-center row-30">
+            <div className="col-md-10 col-lg-7 col-xl-4 text-xl-left"><a href="index.html"><img className="inverse-logo" src="logo.jpg" alt="" style={{width: "80px", borderRadius: "60px"}} alt="" width="208" height="46" /></a></div>
+            <div className="col-md-10 col-lg-7 col-xl-4">
+              <p className="right">©&nbsp;<span className="copyright-year">2024</span> All Rights Reserved. Design&nbsp;by&nbsp;<a href="https://www.templatemonster.com">Gongo Team</a></p>
+            </div>
+            <div className="col-md-10 col-lg-7 col-xl-4 text-xl-right">
+              <ul className="group-xs group-middle"> 
+                <li><a className="icon novi-icon icon-md-middle icon-circle icon-secondary-5-filled mdi mdi-facebook" href="#"></a></li>
+                <li><a className="icon novi-icon icon-md-middle icon-circle icon-secondary-5-filled mdi mdi-twitter" href="#"></a></li>
+                <li><a className="icon novi-icon icon-md-middle icon-circle icon-secondary-5-filled mdi mdi-instagram" href="#"></a></li>
+                <li><a className="icon novi-icon icon-md-middle icon-circle icon-secondary-5-filled mdi mdi-google" href="#"></a></li>
+                <li><a className="icon novi-icon icon-md-middle icon-circle icon-secondary-5-filled mdi mdi-linkedin" href="#"></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import './Header.css'; // Make sure to create and import a CSS file for styling
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header className="section page-header">
+      <div className="rd-navbar-wrap rd-navbar-corporate">
+        <nav className="rd-navbar rd-navbar-original rd-navbar-static">
+          <div className="rd-navbar-collapse-toggle" onClick={toggleMenu}>
+            <span className="hamburger-icon"></span>
+          </div>
+          <div className={`rd-navbar-top-panel rd-navbar-collapse ${isMenuOpen ? 'open' : ''}`}>
+            <div className="rd-navbar-top-panel-inner">
+              <ul className="list-inline">
+                <li className="box-inline list-inline-item">
+                  <span className="icon novi-icon icon-md-smaller icon-secondary mdi mdi-phone"></span>
+                  <ul className="list-comma">
+                    <li><a href="tel:#">+84 379559559</a></li>
+                  </ul>
+                </li>
+                <li className="box-inline list-inline-item">
+                  <span className="icon novi-icon icon-md-smaller icon-secondary mdi mdi-map-marker"></span>
+                  <a href="#">60 Liên Khu 10-11, Bình Trị Đông, Bình Tân, TP. Hồ Chí Minh</a>
+                </li>
+                <li className="box-inline list-inline-item">
+                  <span className="icon novi-icon icon-md-smaller icon-secondary mdi mdi-email"></span>
+                  <a href="mailto:#">info@gongo.vn</a>
+                </li>
+              </ul>
+              <ul className="list-inline">
+                <li className="list-inline-item"><a className="icon novi-icon icon-sm-bigger icon-gray-1 mdi mdi-facebook" href="#"></a></li>
+                <li className="list-inline-item"><a className="icon novi-icon icon-sm-bigger icon-gray-1 mdi mdi-twitter" href="#"></a></li>
+                <li className="list-inline-item"><a className="icon novi-icon icon-sm-bigger icon-gray-1 mdi mdi-instagram" href="#"></a></li>
+                <li className="list-inline-item"><a className="icon novi-icon icon-sm-bigger icon-gray-1 mdi mdi-google-plus" href="#"></a></li>
+                <li className="list-inline-item"><a className="icon novi-icon icon-sm-bigger icon-gray-1 mdi mdi-linkedin" href="#"></a></li>
+              </ul>
+            </div>
+            <div className="rd-navbar-top-panel-inner">
+              <a className="button button-sm button-secondary button-nina" href="https://www.templatemonster.com/website-templates/62466.html" target="_blank" rel="noopener noreferrer">buy template now</a>
+            </div>
+          </div>
+          <div className="rd-navbar-inner">
+            <div className="rd-navbar-panel">
+              <button className="rd-navbar-toggle" onClick={toggleMenu}>
+                <span className="hamburger-icon"></span>
+              </button>
+              <div className="rd-navbar-brand">
+                <a className="brand-name" href="index.html">
+                  <img className="logo-default" src="logo.jpg" alt="" style={{ width: "80px", borderRadius: "60px" }} />
+                  <img className="logo-inverse" src="images/logo-inverse-208x46.png" alt="" width="208" height="46" />
+                </a>
+              </div>
+            </div>
+            <div className={`rd-navbar-aside-center ${isMenuOpen ? 'open' : ''}`}>
+              <div className="rd-navbar-nav-wrap">
+                <ul className="rd-navbar-nav">
+                  <li className="active"><a href="index.html">Home</a></li>
+                  <li><a href="about-us.html">About Us</a></li>
+                  <li><a href="contacts.html">Contacts</a></li>
+                </ul>
+              </div>
+            </div>
+            <div className="rd-navbar-aside-right">
+              <a className="button button-sm button-secondary button-nina" href="#">Book a VISA now</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
