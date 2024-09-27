32032de84e83da7382efa16fa6b953fe36fae7db
Hieu Tran <hieu.tran@vn.defide.co.jp>
2024-09-27 14:48:28 +0700

    [Update] form 1 apply form

--- src/views/apply/Apply.js
+++ src/views/apply/Apply.js
@@ -1,5 +1,5 @@
 import './Apply.css'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { numberOfApplicants, lengthOfVisa, purposeOfTravel, portList, processingTimeList, esimTypeList } from '../../globals/constants'
 import Select from 'react-select';
 import { useForm, Controller } from 'react-hook-form';
@@ -12,8 +12,10 @@ const Apply = () => {
     control,
     formState: { errors },
     handleSubmit,
+    sub
   } = useForm({
-    mode: 'all'
+    mode: 'all',
+    shouldFocusError: true
   });
   const eSimList = esimTypeList.map((i, index) => ({ value: i.value, label: `${i.label} | ${i.price}USD` }))
   const [step, setStep] = useState(1);
@@ -26,6 +28,8 @@ const Apply = () => {
   const [exitPort, setExitPort] = useState(null);
   const [flightNumber, setFlightNumber] = useState('');
   const [processingTime, setProcessingTime] = useState(processingTimeList[0].value);
+  const [promotionCode, setPromotionCode] = useState('');
+  
   const [arrivalFastTrack, setArrivalFastTrack] = useState({
     checked: false,
     arrivalTime: null,
@@ -45,44 +49,51 @@ const Apply = () => {
   
   const handleFocus = (e) => {
     const { target } = e;
-
     if (target) {
       target.readOnly = true;  // -------> this for all others
     }
   };
   function submitHandler(formData){
-    // values are available in formData
+    if (step === 1) setStep(2)
   }
   const onProcessingTimeChanged = (e) => {
     setProcessingTime(Number(e.currentTarget.value))
+  }
+
+  const submitErrorHandler = (errs) => {
+    const errorsvalues = Object.values(errs)
+    if (errorsvalues.length) {
+      let firstErrorElement = document.getElementsByName(errorsvalues[0].ref.name)[0];
+      firstErrorElement.parentElement.scrollIntoView({ behavior: `smooth`, block: 'center'});
+    }
   }
 
   return (
     <div className='apply'>
       <div className="container-wide">
         <div className='apply_stepper row'>
-          <div className={'apply_stepper_item col-md-2 ' + (step >= 1 ? 'active' : '')}>
+          <div className={'apply_stepper_item col-md-2 col-3 ' + (step >= 1 ? 'active' : '')}>
             <div className='apply_stepper_item_number'>
               <span>1</span>
               <div className='apply_stepper_item_number--line'></div>
             </div>
             <div className='apply_stepper_item_text'>Service Type</div>
           </div>
-          <div className={'apply_stepper_item col-md-2 ' + (step >= 2 ? 'active' : '')}>
+          <div className={'apply_stepper_item col-md-2 col-3 ' + (step >= 2 ? 'active' : '')}>
             <div className='apply_stepper_item_number'>
               <span>2</span>
               <div className='apply_stepper_item_number--line'></div>
             </div>
             <div className='apply_stepper_item_text'>Your Information</div>
           </div>
-          <div className={'apply_stepper_item col-md-2 ' + (step >= 3 ? 'active' : '')}>
+          <div className={'apply_stepper_item col-md-2 col-3 ' + (step >= 3 ? 'active' : '')}>
             <div className='apply_stepper_item_number'>
               <span>3</span>
               <div className='apply_stepper_item_number--line'></div>
             </div>
             <div className='apply_stepper_item_text'>Payment</div>
           </div>
-          <div className={'apply_stepper_item col-md-2 ' + (step >= 4 ? 'active' : '')}>
+          <div className={'apply_stepper_item col-md-2 col-3 ' + (step >= 4 ? 'active' : '')}>
             <div className='apply_stepper_item_number'>
               <span>4</span>
             </div>
@@ -92,7 +103,7 @@ const Apply = () => {
         <div className='container'>
           <div className='apply_body row'>
             <div className='apply_body_form col-md-8'>
-              <form onSubmit={handleSubmit(submitHandler)} >
+              <form onSubmit={handleSubmit(submitHandler, submitErrorHandler)} >
                 <div className='apply_body_form--title'>Visa application form</div>
                 <div className='row apply_body_form_input'>
                   <div className="col-lg-6 mb-3">
@@ -164,7 +175,7 @@ const Apply = () => {
                       />
                     </div>
                   </div>
-                  <div className="col-lg-3 mb-3">
+                  <div className="col-lg-3 col-6 mb-3">
                     <div className='apply_body_form_input--label'>
                       Entry date
                       <span className='apply_body_form_input--required'>*</span>
@@ -200,7 +211,7 @@ const Apply = () => {
                       { errors.entryDate ? (<span className='error-message'>{ errors.entryDate.message }</span>) : null }
                     </div>
                   </div>
-                  <div className="col-lg-3 mb-3">
+                  <div className="col-lg-3 col-6 mb-3">
                     <div className='apply_body_form_input--label'>
                       Exit date
                       <span className='apply_body_form_input--required'>*</span>
@@ -246,7 +257,7 @@ const Apply = () => {
                     <div className='input-group'>
                       <Controller
                         control={control}
-                        defaultValue={numberApplicants}
+                        defaultValue={arrivalPort}
                         name="arrivalPort"
                         rules={{
                           required: {
@@ -281,7 +292,7 @@ const Apply = () => {
                     <div className='input-group'>
                       <Controller
                         control={control}
-                        defaultValue={numberApplicants}
+                        defaultValue={exitPort}
                         name="exitPort"
                         rules={{
                           required: {
@@ -350,7 +361,7 @@ const Apply = () => {
                   </div>
                 </div>
                 <div className='apply_body_form--title mt-3'>ADD-ON SERVICES</div>
-                <div className={'apply_body_form_input ' + (!arrivalFastTrack.checked ? 'mb-3' : '')}>
+                <div className='apply_body_form_input mb-3'>
                   <div className='checkbox_group'>
                     <input type="checkbox"
                       defaultChecked={arrivalFastTrack.checked}
@@ -362,7 +373,7 @@ const Apply = () => {
                     </label>
                   </div>
                   { arrivalFastTrack.checked ? (
-                    <div className='row mt-4'>
+                    <div className='row mt-2'>
                       <div className="col-lg-3 mb-3">
                         <div className='apply_body_form_input--label'>
                           Arrival time
@@ -383,6 +394,7 @@ const Apply = () => {
                               <TimePicker
                                 name={name}
                                 showSecond={false}
+                                value={arrivalFastTrack.arrivalTime}
                                 onChange={(value) => {
                                   setArrivalFastTrack({...arrivalFastTrack, arrivalTime: value})
                                   onChange(value)
@@ -470,7 +482,7 @@ const Apply = () => {
                     </div>
                   ) : null }
                 </div>
-                <div className={'apply_body_form_input ' + (!carPickup.checked ? 'mb-3' : '')}>
+                <div className='apply_body_form_input mb-3'>
                   <div className='checkbox_group'>
                     <input type="checkbox"
                       defaultChecked={carPickup.checked}
@@ -482,7 +494,7 @@ const Apply = () => {
                     </label>
                   </div>
                   { carPickup.checked ? (
-                    <div className='row mt-4'>
+                    <div className='row mt-2'>
                       <div className="col-lg-6 mb-3">
                         <div className='apply_body_form_input--label'>
                           Drop off point
@@ -554,7 +566,7 @@ const Apply = () => {
                     </div>
                   ) : null }
                 </div>
-                <div className='apply_body_form_input'>
+                <div className='apply_body_form_input mb-5'>
                   <div className='checkbox_group'>
                     <input type="checkbox"
                       defaultChecked={eSIM.checked}
@@ -566,7 +578,7 @@ const Apply = () => {
                     </label>
                   </div>
                   { eSIM.checked ? (
-                    <div className='row mt-4'>
+                    <div className='row mt-2'>
                       <div className="col-lg-6 mb-3">
                         <div className='apply_body_form_input--label'>
                           Quantity
@@ -640,9 +652,90 @@ const Apply = () => {
                     </div>
                   ) : null }
                 </div>
+                <div className='apply_body_form_footer'>
+                  <button type='submit' className='btn btn-booking btn-danger'>Next &gt;</button>
+                </div>
               </form>
             </div>
-            <div className='apply_body_info col-md-4'></div>
+            <div className='col-md-4'>
+              <div className='apply_body_info'>
+                <div className='apply_body_info--title'>REVIEW YOUR ORDER</div>
+                <div className='apply_body_info--type'>
+                  <img src='/images/fly.svg' alt='fly' />EVisa:
+                </div>
+                <ul className='apply_body_info--listItem'>
+                  <li><strong>Purpose of travel:</strong>{purposeTravel ? purposeTravel.label : ''}</li>
+                  <li><strong>Length of visa:</strong>{lengthVisa ? lengthVisa.label : ''}</li>
+                  <li><strong>Entry date:</strong>{entryDate ? dayjs(entryDate).format('YYYY-MM-DD') : ''}</li>
+                  <li><strong>Exit date:</strong>{exitDate ? dayjs(exitDate).format('YYYY-MM-DD') : ''}</li>
+                  <li><strong>Arrival port:</strong>{arrivalPort ? arrivalPort.label : ''}</li>
+                  <li><strong>Exit through checkpoint:</strong>{exitPort ? exitPort.label : ''}</li>
+                  <li><strong>Number of Applicants:</strong>{numberApplicants ? numberApplicants.label : ''}</li>
+                  <li><strong>Service fee:</strong><div className='text-right'>0 USD</div></li>
+                  <li><strong>Processing fee:</strong><div className='text-right'>0 USD</div></li>
+                  <li><strong>Admin & Government fee:</strong><div className='text-right'>0 USD</div></li>
+                </ul>
+                <hr  />
+                { arrivalFastTrack.checked || carPickup.checked || eSIM.checked ? (<>
+                  <div className='apply_body_info--type mb-2'>
+                    ADD-ON SERVICES
+                  </div>
+                  { arrivalFastTrack.checked ? (<>
+                    <div className='apply_body_info--type'>
+                      <img src='/images/fly.svg' alt='fly' />Arrival Fast track:
+                    </div>
+                    <ul className='apply_body_info--listItem'>
+                      <li><strong>Arrival time:</strong>{arrivalFastTrack.arrivalTime ? arrivalFastTrack.arrivalTime.format('HH:mm') : ''}</li>
+                      <li><strong>Flight number:</strong>{arrivalFastTrack.flightNumber || ''}</li>
+                      <li><strong>Type:</strong>{arrivalFastTrack.type ? arrivalFastTrack.type.label : ''}</li>
+                    </ul>
+                  </>) : null }
+                  { carPickup.checked ? (<>
+                    <div className='apply_body_info--type'>
+                      <img src='/images/fly.svg' alt='fly' />Car Pick up:
+                    </div>
+                    <ul className='apply_body_info--listItem'>
+                      <li><strong>Drop off point:</strong>{carPickup.dropOffPoint || ''}</li>
+                      <li><strong>Type of Car:</strong>{carPickup.typeOfCar ? carPickup.typeOfCar.label : ''}</li>
+                    </ul>
+                  </>) : null }
+                  { eSIM.checked ? (<>
+                    <div className='apply_body_info--type'>
+                      <img src='/images/fly.svg' alt='fly' />eSIM
+                    </div>
+                    <ul className='apply_body_info--listItem'>
+                      <li><strong>Quantity:</strong>{eSIM.quantity ? eSIM.quantity.label : ''}</li>
+                      <li><strong>Type:</strong>{eSIM.type ? eSIM.type.label : ''}</li>
+                    </ul>
+                  </>) : null }
+                </>) : null }
+                <div className='apply_body_info_promotionCode'>
+                  <div className='apply_body_info_promotionCode--title'>Promotion code</div>
+                  <div className='apply_body_info_promotionCode_inputGroup'>
+                    <input type='text' value={promotionCode} onChange={(e) => setPromotionCode(e.currentTarget.value)}/>
+                    <button type='button' className='btn btn-danger'>Apply</button>
+                  </div>
+                </div>
+                <div className='apply_body_info_fee'>
+                  <div className='apply_body_info_fee_item'>
+                    <span className='apply_body_info_fee_item--label'>Subtotal:</span>
+                    <span className='apply_body_info_fee_item--value'>0</span>
+                  </div>
+                  <div className='apply_body_info_fee_item'>
+                    <span className='apply_body_info_fee_item--label'>Discount:</span>
+                    <span className='apply_body_info_fee_item--value'>0</span>
+                  </div>
+                  <div className='apply_body_info_fee_item total'>
+                    <span className='apply_body_info_fee_item--label'>Total:</span>
+                    <span className='apply_body_info_fee_item--value'>0</span>
+                  </div>
+                  <div className='apply_body_info_fee_item convert'>
+                    <span className='apply_body_info_fee_item--label'>1 USD = 25,900 VND</span>
+                    <span className='apply_body_info_fee_item--value'>0</span>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
